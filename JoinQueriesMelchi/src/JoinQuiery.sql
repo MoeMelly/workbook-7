aa2ab4d6bde87fd77306559d2bd3e3dfee53c9e2
MoeMelly <mellyscos@gmail.com>
2025-06-04 14:53:04 -0500

    Delete JoinQueriesMelchi directory

--- JoinQueriesMelchi/src/JoinQuiery.sql
+++
@@ -1,9 +0,0 @@
-use northwind;
--- question 1--
-SELECT products.ProductID, products.ProductName, products.UnitPrice, categories.CategoryName FROM products
-inner join categories on
-products.CategoryID = categories.CategoryID
-order by categoryName, productName;
--- question 3 --
-SELECT products.ProductID, products.ProductName, 
-
